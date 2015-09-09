da4b7c2bbb61c16fe8b12771336aa5f4d4a598a3
steviedc69 <steven_decock@hotmail.com>
2015-09-09 16:45:12 +0200

    Revert "Solution Exercise 4"
    This reverts commit b0c85e12a80095f0cb8d35176dcb9f3372bce205.

--- ejb-module/src/test/java/com/realdolmen/course/persistence/PassengerPersistenceTest.java
+++ ejb-module/src/test/java/com/realdolmen/course/persistence/PassengerPersistenceTest.java
@@ -2,7 +2,6 @@ package com.realdolmen.course.persistence;
 
 
 import com.realdolmen.course.domain.Passenger;
-import com.realdolmen.course.domain.PassengerType;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -19,40 +18,30 @@ public class PassengerPersistenceTest extends DataSetPersistenceTest {
 
     @Test
     public void passengerCanBePersisted() throws Exception {
-        Passenger p = new Passenger("02123321","john","Doe",15,"15/02/1990", PassengerType.ECONOMY);
+        Passenger p = new Passenger("02123321","john","Doe",15);
         entityManager().persist(p);
         assertNotNull(p.getId());
     }
 
     @Test(expected = PersistenceException.class)
     public void passengerCanNotBePersistedWithoutSsn() throws Exception {
-        Passenger p = new Passenger(null, "John","Smith",15,"15/02/1990", PassengerType.ECONOMY);
+        Passenger p = new Passenger(null, "John","Smith",15);
         entityManager().persist(p);
-        entityManager().flush();
     }
     @Test(expected = PersistenceException.class)
     public void passengerCanNotBePersistedWithoutFirstName() throws Exception{
-        Passenger p = new Passenger("122313213",null,"Smithers",11,"15/02/1990", PassengerType.ECONOMY);
+        Passenger p = new Passenger("122313213",null,"Smithers",11);
         entityManager().persist(p);
-        entityManager().flush();
     }
     @Test(expected = PersistenceException.class)
     public void passengerCanNotBePersistedWithoutLastName() throws Exception{
-        Passenger p = new Passenger("122313213","jan",null,11,"15/02/1990", PassengerType.ECONOMY);
+        Passenger p = new Passenger("122313213","jan",null,11);
         entityManager().persist(p);
-        entityManager().flush();
     }
 
     @Test
     public void passengerCanBeRetrievedById() throws Exception {
 
-        assertEquals("02123321", entityManager().find(Passenger.class, 1L).getSsn());
-
-    }
-
-    @Test
-    public void passengerAgeCanBeCalculated() throws Exception{
-        Passenger p = new Passenger("122313213","jan","De Boel",11,"15/02/1990", PassengerType.ECONOMY);
-        assertEquals(25,p.getAge());
+        assertEquals("02123321",entityManager().find(Passenger.class,1L).getSsn());
     }
 }
